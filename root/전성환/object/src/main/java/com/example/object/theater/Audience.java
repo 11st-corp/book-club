92894575fd739c685edc527e72bc0a1917cd8127
Sunghwan-DS <jeonsung02@gmail.com>
2021-06-27 23:49:29 +0900

    Object Chapter 1_3_6

--- root/전성환/object/src/main/java/com/example/object/theater/Audience.java
+++ root/전성환/object/src/main/java/com/example/object/theater/Audience.java
@@ -7,7 +7,14 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.minusAmount(ticket.getFee());
+            bag.setTicket(ticket);
+            return ticket.getFee();
+        }
     }
 }

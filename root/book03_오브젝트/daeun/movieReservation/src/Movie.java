add71bc0d57f7ece3759b0e3503f5dae39b6df0d
jeongdaeun98 <uneap@sk.com>
2021-07-05 11:54:06 +0900

    add ch 02 객체지향 프로그래밍

---
+++ root/book03_오브젝트/daeun/movieReservation/src/Movie.java
@@ -0,0 +1,23 @@
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration duration;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DefaultDiscountPolicy discountPolicy) {
+        this.title = title;
+        this.duration = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
---
+++ root/book03_오브젝트/daeun/movieReservation/src/NoneDiscountPolicy.java
@@ -0,0 +1,6 @@
+public class NoneDiscountPolicy implements DiscountPolicy {
+    @Override
+    public Money calculateDiscountAmount(Screening screening) {
+        return Money.ZERO;
+    }
+}
